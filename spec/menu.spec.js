f8c23fb3a1f75cc1e8896c2dba15b7ae526663f9
Petra Boldi <boldi.petra@gmail.com>
2021-03-22 17:58:02 +0100

    Extension 1, test cases 5-8

--- spec/menu.spec.js
+++ spec/menu.spec.js
@@ -40,43 +40,38 @@ describe('Testing print menu function', function() {
         expect(bobsMenu.printMenu()).toBe(`BOBS BAGELS MENU\n${bagel.SKU} - ${bagel.name} - ${bagel.price}\n${bagel2.SKU} - ${bagel2.name} - ${bagel2.price}\n${bagel3.SKU} - ${bagel3.name} - ${bagel3.price}`)
     })
 
-    // test.it('Doesnt print menu when it is empty', () =>{
-    //     bobsMenu = new Menu('bobs bagels');
-
-    //     result = bobsMenu.printMenu();
-        
-    //     test.expect(result).toEqual(`Sorry! There are no items on the menu`);
-    // })
+    it('Doesnt print menu when it is empty', function(){
+        bobsMenu = new Menu('bobs bagels');
+        expect(bobsMenu.printMenu()).toBe(`Sorry! There are no items on the menu`);
+     })
 })
 
 
 
-// test.describe('Testing menu.removeItem() functnon', () => {
-//     test.it('Removes item from the menu', () => {
-//         bobsMenu = new Menu('bobs bagels');
-//         bagel = new Bagel();
-//         bagel2 = new Bagel('BGLO');
-//         bagel4 = new Bagel('BGLO');
+describe('Testing menu.removeItem() function', function(){
+    it('Removes item from the menu', function(){
+        bobsMenu = new Menu('bobs bagels');
+        bagel = new Bagel();
+        bagel2 = new Bagel('BGLO');
+        bagel4 = new Bagel('BGLO');
 
-//         bobsMenu.addToMenu(bagel);
-//         bobsMenu.addToMenu(bagel2);
-//         test.expect(bobsMenu.removeFromMenu(bagel4).length).toEqual(1);
-//     })
+        bobsMenu.addToMenu(bagel);
+        bobsMenu.addToMenu(bagel2);
+        expect(bobsMenu.removeFromMenu(bagel4).length).toBe(1);
+    })
 
-//     test.it('Cannot remove item.SKU that is not in the menu', () => {
-//         bagel3 = new Bagel('BGLO');
-//         test.expect(bobsMenu.removeFromMenu(bagel3)).toEqual(`Cannot remove item that is not on the menu`);
-//     })
-// })
+    it('Cannot remove item.SKU that is not in the menu', function() {
+        bagel3 = new Bagel('BGLO');
+        expect(bobsMenu.removeFromMenu(bagel3)).toBe(`Cannot remove item that is not on the menu`);
+    })
+})
 
 
-// test.describe('Testing offers class, adding and removing', () => {
-//     test.it('Check that you can add an offer to menu', () => {
-//         bobsMenu = new Menu('bobs bagels');
-//         offer = new Offer('BGLO', 6, 2.49);
-
-//         result = bobsMenu.addOffer(offer);
-
-//         test.expect(result[0]).toEqual(offer);
-//     })
-// })
+describe('Testing offers class, adding and removing', function(){
+    it('Check that you can add an offer to menu', function(){
+        bobsMenu = new Menu('bobs bagels');
+        offer = new Offer('BGLO', 6, 2.49);
+        result = bobsMenu.addOffer(offer);
+        expect(result[0]).toBe(offer);
+    })
+})
